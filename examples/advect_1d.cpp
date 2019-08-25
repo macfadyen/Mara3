9c200f2cf5743c680c1307fc5fcc7e70887df0ad
macfadyen <macfadyen@nyu.edu>
2019-08-25 06:56:17 -0400

    1d advection code

--- examples/advect_1d.cpp
+++ examples/advect_1d.cpp
@@ -1,6 +1,6 @@
 /**
  ==============================================================================
- Copyright 2019, Magdalena Siwek
+ Copyright 2019, Andrew MacFadyen
 
  Permission is hereby granted, free of charge, to any person obtaining a copy of
  this software and associated documentation files (the "Software"), to deal in
@@ -23,16 +23,170 @@
  ==============================================================================
 */
 
-
-
-
-#include "core_ndarray.hpp"
+/**
+ * @brief Solve linear advection equation for scalar field u(x,t) 
+ *
+ * u_t + div (a * u) = 0
+ *
+ * where a is a constant advection speed
+ */
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <cmath>
+#include <cassert>
 #include "core_ndarray_ops.hpp"
+#include "core_hdf5.hpp"
+#include "app_serialize.hpp"
+#include "app_config.hpp"
+#include "app_filesystem.hpp"
 
+using namespace nd;
 
-
-
-int main()
+struct state_t
 {
-    return 0;
+	int    iteration;
+	double time;
+	nd::shared_array<double,1> x_vertices;      
+	nd::shared_array<double,1> u; 
+};
+
+struct solver_data_t
+{
+    double advection_speed;
+    double cfl;
+    double gaussian_width;
+    int    rk_order;
+};
+
+double gaussian (double x)
+{
+	return std::exp(-0.5 * x * x);
+}
+
+state_t initialize_state(const mara::config_t cfg)
+{
+	auto nx = cfg.get_int("resolution");
+	auto x_vertices = linspace(cfg.get_double("xmin"),cfg.get_double("xmax"), nx + 1);
+	auto x_centers = x_vertices | midpoint_on_axis(0);
+	auto u = x_centers | map([] (auto xi) { return gaussian(xi); });
+	return{0, 0.0, x_vertices.shared(), u.shared()};
+}
+
+solver_data_t create_solver_data(const mara::config_t cfg)
+{
+    return {
+        cfg.get_double("advection_speed"),
+        cfg.get_double("cfl"),
+        cfg.get_double("gaussian_width"),
+    };
+}
+
+void write_checkpoint(const state_t& state, std::string fname)
+{
+	auto h5f = h5::File(fname, "w" );
+
+	auto x_centers = state.x_vertices | midpoint_on_axis(0);	
+
+	h5f.write("iteration", state.iteration);
+	h5f.write("time",      state.time);
+	h5f.write("x_centers", x_centers.shared());
+	h5f.write("u",         state.u.shared());
+
+	h5f.close();
+
+	std::cout << "Wrote " << fname << std::endl;
+}
+
+auto update_u (const state_t& state, double advection_speed, double dt)
+{
+	auto u = state.u | extend_periodic_on_axis(0);
+	auto F = u * advection_speed;
+	auto Fl = F | select_axis(0).from(0).to(1).from_the_end();
+	auto Fr = F | select_axis(0).from(1).to(0).from_the_end();	
+	auto Fhat = advection_speed > 0.0 ? Fl : Fr;
+	auto delta_Fhat =     Fhat | difference_on_axis(0);
+	auto dx = state.x_vertices | difference_on_axis(0);
+
+	return state.u - delta_Fhat * dt / dx | to_shared();
+}
+/*
+auto next_state(const state_t& state, const solver_data_t& solver_data)
+{
+	double dt = 0.025;
+
+	auto s0 = state;
+
+        switch (solver_data.rk_order)
+        {
+            case 1:
+            {
+                return    update_u(s0, dt, solver_data.advection_speed);
+            }
+            case 2:
+            {
+                auto s1 = update_u(s0, dt, solver_data.advection_speed);
+                auto s2 = update_u(s1, dt, solver_data.advection_speed);
+                
+                return (s0 + s2) * 0.5;
+            }
+        }
+}
+
+solution_t advance(const state_t& state, const solver_data_t& solver_data, double dt)
+{
+	return{iteration+1, time+dt, x_vertices, update_u(state,dt,advection_speed)}
+}
+*/
+
+state_t new_state(const state_t& state, const solver_data_t& solver_data)
+{
+	double dt = 0.01;
+	auto    a = solver_data.advection_speed;
+
+	return{state.iteration+1, state.time+dt, state.x_vertices, update_u(state, a, dt)};
+}
+
+
+int main(int argc, const char* argv[])
+{
+    auto cfg_template = mara::make_config_template()
+    .item("resolution",      400)
+    .item("tfinal",         10.0)
+    .item("xmin",           -5.0)
+    .item("xmax",            5.0)
+    .item("advection_speed", 0.5)
+    .item("gaussian_width",  0.2)
+    .item("cfl",             0.5)
+    .item("rk_order",          1)    
+    .item("ichkpt",            0)        
+    .item("outdir", "advect1d_data");
+
+    auto args = mara::argv_to_string_map(argc, argv);
+    auto run_config = cfg_template.create().update(args);
+    mara::pretty_print(std::cout, "config", run_config);
+    auto solver_data = create_solver_data(run_config);
+    auto state = initialize_state(run_config);
+//    write_diagnostics(state, "diagnostics_0000.h5");
+
+    while( state.time < run_config.get_double("tfinal") )
+    {
+    	state = new_state(state, solver_data);
+		
+	//	printf( " %d : t = %0.2f \n", state.iteration, state.time );
+    	std::printf("[%04d] time=%3.7lf\n",state.iteration,state.time);	    	
+
+    	if (state.iteration%10 == 0)
+    	{
+    		auto outdir = run_config.get_string("outdir");
+    		auto ichkpt = run_config.get_int   ("ichkpt");    		
+    		auto fname  = mara::filesystem::join(outdir, mara::create_numbered_filename("chkpt", ichkpt, "h5"));
+    	    write_checkpoint(state, fname);
+    	    run_config = run_config.set("ichkpt", ++ichkpt );
+    	}
+    }
+
+
 }
